34ee5da7834d2c9e56d33fa49ecf4ceefc01d404
ELGOKU23 <120664905+ELGOKU23@users.noreply.github.com>
2024-07-02 21:23:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -176,4 +176,5 @@ def generate_syntax_tree():
         return jsonify({"success": False})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    app.run(host='0.0.0.0', port=5000, debug=True)
